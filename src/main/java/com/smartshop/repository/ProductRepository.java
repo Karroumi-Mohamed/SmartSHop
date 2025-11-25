b24bb33459ae658e0e50d2a47554e1dab0ddb527
Mohamed Karroumi <karroumim2@gmail.com>
2025-11-25 16:31:50 +0100

    feat: Add repository interfaces for Admin, Client, Order, Payment, Product, PromoCode, and User entities

---
+++ src/main/java/com/smartshop/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.smartshop.repository;
+
+import com.smartshop.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByDeletedFalse(Pageable pageable);
+}
